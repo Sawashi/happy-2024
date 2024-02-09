5e4a4ed930958a55abba0b6bb440caf4dbf2ce6f
Kiet <92447748+Sawashi@users.noreply.github.com>
2024-02-09 22:25:06 +0700

    reduce complexity

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,9 @@ import { useEffect, useState } from "react";
 import Countdown from "react-countdown";
 
 function App() {
-  const [newYearMessage, setNewYearMessage] = useState(["Bye 2023!"]);
   const particleInitialization = async (engine) => {
     await loadFireworksPreset(engine);
   };
-
-  function timeLeft() {
-    const newYearDate = new Date("January 1, 2024 00:00:00").getTime();
-    const nowDate = new Date().getTime();
-    const remainingTime = newYearDate - nowDate;
-    return remainingTime;
-  }
-  useEffect(() => {
-    setNewYearMessage(["Happy new year 2024!"]);
-  }, []);
   return (
     <>
       <Particles
@@ -28,12 +17,7 @@ function App() {
 
       <div className="flex flex-col justify-center items-center min-h-screen gap-4">
         <span className="text-white text-4xl font-bold z-50">
-          <Typewriter
-            words={newYearMessage}
-            loop={false}
-            cursorStyle={"_"}
-            cursor
-          />
+          Happy new year 2024!
         </span>
         <div className="z-50 text-white font-bold text-2xl"></div>
       </div>
